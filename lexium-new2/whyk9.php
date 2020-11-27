8ce11208b612d8cc4d3da1df7927684f6b99388f
IsaacXicotencatl <isaac@lexiumonline.com>
2020-11-27 15:27:52 -0600

    cambios

--- lexium-new2/whyk9.php
+++ lexium-new2/whyk9.php
@@ -80,7 +80,7 @@
                          </div>
                             
 	     	  	   	    	   <div class="w-100 p-3 whytxt">
-	     	  	   	    	     	  <h6 style="font-family: 'HarmoniaSansW01-Regular'; color: #23215b; font-size: 1.3em;">Mejora la oferta </br> acádemica</h6>
+	     	  	   	    	     	  <h6 style="font-family: 'HarmoniaSansW01-Regular'; color: #23215b; font-size: 1.3em;">Mejora la oferta </br> académica</h6>
 	     	  	   	    	   </div>
 	     	  	   	    	 </div>
 	     	  	   	    </div>
