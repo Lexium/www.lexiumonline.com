701ce8227d3b55a1a0b7c173b0fabfffbcde9467
IsaacXicotencatl <isaac@lexiumonline.com>
2020-12-10 23:58:42 -0600

    cambios

--- lexiumv3/sip.php
+++ lexiumv3/sip.php
@@ -75,7 +75,7 @@ include 'header.php' ?>
         </button>
       </div>
       <div class="modal-body">
-         <iframe width="100%" height="360" src="https://www.youtube.com/embed/McOLQRZHFKw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+         <iframe src="https://player.vimeo.com/video/489617317?title=0&byline=0" width="100%" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
       </div>
     </div>
   </div>
