a1a0fc94fc19577fe20b3d684a8388cea09b7c43
eloymvzz <eloymvzz@gmail.com>
2020-12-15 15:59:21 -0600

    Se activó el tracking del formulario para Hot Jar

--- header.php
+++ header.php
@@ -62,6 +62,24 @@
   gtag('config', 'AW-784291087');
 </script>
 
+<?php if($archivo_previo == 'contacto.php')
+{
+?>
+<!-- Hotjar Tracking Code for https://www.lexiumonline.com/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1334117,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<?php
+}?>
+
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
