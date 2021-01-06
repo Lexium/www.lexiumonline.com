7d13bf867f27f1a2a715f4c01dd24fb6c56c6a58
IsaacXicotencatl <isaac@lexiumonline.com>
2021-01-06 10:54:25 -0600

    ventana de resolucion

--- js/conf.js
+++
@@ -1,7 +0,0 @@
-if (screen.width < 1024) 
-   console.log ("Pequeña") 
-else 
-   if (screen.width < 1280) 
-      console.log ("Mediana") 
-   else 
-      console.log ("Grande") 
--- resolucion.php
+++ resolucion.php
@@ -1,6 +1,7 @@
 <div class="resolucion container pt-3 pb-3">
      <span>Para una mejor navegacion de este sitio le recomendamos poner su dispositivo en forma orizontal </span> <br>
-     <img class="img-fluid" src="resources/rotatedevice.gif">
+     <img class="img-fluid" src="resources/rotatedevice.gif"><br>
+     <!-- <a class="btn btn-primary closewindow" href="">Cerrar</a> -->
 </div>
 
 
@@ -8,9 +9,17 @@
 
 <style type="text/css">
 	
-	 .resolucion{ width: 100%; background-color: yellow; position: absolute; z-index: 100000;  display: none; text-align: center; margin-top: 50%;}
+	 .resolucion{ width: 100%; background-color: yellow; position: fixed; z-index: 100000;  display: none; text-align: center; margin-top: 50%;}
      .resolucion img{ width: 15%; }
+	 .resolucion a{ width: 25%!important; }
 	 @media (max-width: 552px) {
-  .resolucion{ display: block; }
-}
+       .resolucion{ display: block; }
+     }
 </style>
+
+
+<!-- <script type="text/javascript">
+	  $('.closewindow').click(function(){
+         $('.resolucion').css('display', 'none');
+      });
+</script> -->
